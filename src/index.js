a99d6c4f16180db2f6e57ad5033ace0021726d22
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-02-15 17:06:23 +0200

    ht11

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './css/styles.css';
+
+async function fetchCountries() {
+  const response = await fetch(`https://pixabay.com/api/?key=25718667-d0b548046b545cf0dd46ad07c&q=yellow+flowers&image_type=photo&pretty=true`);
+  if (!response.ok) {
+    throw new Error(response.status);
+  } else {
+    const countries = await response.json();
+    return countries;
+  }
+};
+
+async function onSearch() {
+    try {
+        const countries = await fetchCountries();
+        console.log(countries);
+    } catch {
+        console.log("ошибка");
+    }
+};
+
+onSearch();
