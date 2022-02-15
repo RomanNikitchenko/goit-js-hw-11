e7dcd292c3efc57a96102bc4bc90157df4207473
RomanNikitchenko <88657590+RomanNikitchenko@users.noreply.github.com>
2022-02-15 15:07:28 +0000

    Deploying to gh-pages from @ RomanNikitchenko/goit-js-hw-11@a99d6c4f16180db2f6e57ad5033ace0021726d22 🚀

--- src.95718ee1.js
+++ src.95718ee1.js
@@ -1,6 +1,6 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"krre":[function(require,module,exports) {
 
 },{}],"Focm":[function(require,module,exports) {
-"use strict";require("./css/styles.css");
+"use strict";async function t(){const t=await fetch("https://pixabay.com/api/?key=25718667-d0b548046b545cf0dd46ad07c&q=yellow+flowers&image_type=photo&pretty=true");if(t.ok){return await t.json()}throw new Error(t.status)}async function o(){try{const o=await t();console.log(o)}catch{console.log("ошибка")}}require("./css/styles.css"),o();
 },{"./css/styles.css":"krre"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11/src.cdefb269.js.map
+//# sourceMappingURL=/goit-js-hw-11/src.95718ee1.js.map
