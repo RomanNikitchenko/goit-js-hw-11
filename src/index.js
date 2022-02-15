3253e152d0a50557bfb0786cbf8a5fbcb13fe368
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-02-15 21:56:47 +0200

    ht 11

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import './css/styles.css';
 
 async function fetchCountries() {
-  const response = await fetch(`https://pixabay.com/api/?key=25718667-d0b548046b545cf0dd46ad07c&q=dog&image_type=photo&per_page=42&page=2`);
+  const response = await fetch(`https://pixabay.com/api/?key=25718667-d0b548046b545cf0dd46ad07c&q=dog&image_type=photo&per_page=5&page=1`);
   if (!response.ok) {
     throw new Error(response.status);
   } else {
-    const countries = await response.json();
-    return countries;
+    const picture = await response.json();
+    return picture;
   }
 };
 
 async function onSearch() {
-    try {
-        const countries = await fetchCountries();
-        console.log(countries);
-    } catch {
-        console.log("ошибка");
-    }
+  try {
+    const picture = await fetchCountries();
+    const { total, totalHits, hits } = picture;
+    console.log(hits);
+  } catch {
+    console.log("ошибка");
+  }
 };
 
 onSearch();
