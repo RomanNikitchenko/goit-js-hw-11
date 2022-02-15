c6d759c384c8d60761a3d75b7d70e594000c09d5
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-02-15 22:55:44 +0200

    ht11

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './css/styles.css';
 
-async function fetchCountries() {
-  const response = await fetch(`https://pixabay.com/api/?key=25718667-d0b548046b545cf0dd46ad07c&q=dog&image_type=photo&per_page=5&page=1`);
+const form = document.querySelector("#search-form");
+
+form.addEventListener("submit", onSearch);
+
+async function fetchPixabay() {
+  const response = await fetch(`https://pixabay.com/api/?key=25718667-d0b548046b545cf0dd46ad07c&q=dog&image_type=photo&safesearch=true&per_page=5&page=1`);
   if (!response.ok) {
     throw new Error(response.status);
   } else {
@@ -10,14 +14,17 @@ async function fetchCountries() {
   }
 };
 
-async function onSearch() {
+async function onSearch(e) {
+  e.preventDefault();
+  const { elements } = e.currentTarget;
+  const { searchQuery } = elements;
+  console.log(searchQuery.value);
+
   try {
-    const picture = await fetchCountries();
+    const picture = await fetchPixabay();
     const { total, totalHits, hits } = picture;
     console.log(hits);
   } catch {
     console.log("ошибка");
   }
 };
-
-onSearch();
