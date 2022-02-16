78a15d943898c306b82df4f4fddc9f9632116c9a
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-02-16 22:31:07 +0200

    ht11

--- src/index.js
+++ src/index.js
@@ -5,29 +5,38 @@ const form = document.querySelector("#search-form");
 const btnLoadMore = document.querySelector(".load-more");
 const gallery = document.querySelector(".gallery");
 
-
 let name = '';
 let page = 1;
 
+const doStuff = async (name, page) => {
+  try {
+    const picture = await fetchPixabay(name, page);
+    const { total, totalHits, hits } = picture;
+    renderPosts(hits);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 form.addEventListener("submit", onSearch);
 
 async function onSearch(e) {
   e.preventDefault();
-  page = 1;
-  gallery.innerHTML = '';
 
   const { elements } = e.currentTarget;
   const { searchQuery } = elements;
+
+  if (!searchQuery.value) {
+    return
+  };
+
+  gallery.innerHTML = '';
+
   name = searchQuery.value;
 
-  try {
-    const picture = await fetchPixabay(name, page);
-    const { total, totalHits, hits } = picture;
-    renderPosts(hits);
-  } catch {
-    console.log("ошибка");
-  };
+  page = 1;
+
+  doStuff(name, page);
 
   searchQuery.value = '';
 };
@@ -37,9 +46,8 @@ btnLoadMore.addEventListener('click', onLoadMore);
 
 async function onLoadMore() {
   page += 1;
-  const pictureMore = await fetchPixabay(name, page);
-  const { total, totalHits, hits } = pictureMore;
-  renderPosts(hits)
+
+  doStuff(name, page);
 };
 
 
