14264fcacd3ff5954e7b7d17a70820fb72167340
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-02-16 12:41:17 +0200

    ht 11

---
+++ src/fetchPixabayserver.js
@@ -0,0 +1,9 @@
+export default async function fetchPixabay(name) {
+  const response = await fetch(`https://pixabay.com/api/?key=25718667-d0b548046b545cf0dd46ad07c&q=${name}&image_type=photo&safesearch=true&per_page=40&page=1`);
+  if (!response.ok) {
+    throw new Error(response.status);
+  } else {
+    const picture = await response.json();
+    return picture;
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
 import './css/styles.css';
+import fetchPixabay from './fetchPixabayserver';
 
 const form = document.querySelector("#search-form");
+const btnLoadMore = document.querySelector(".load-more");
+
+
+let name = '';
+
 
 form.addEventListener("submit", onSearch);
 
-async function fetchPixabay() {
-  const response = await fetch(`https://pixabay.com/api/?key=25718667-d0b548046b545cf0dd46ad07c&q=dog&image_type=photo&safesearch=true&per_page=5&page=1`);
-  if (!response.ok) {
-    throw new Error(response.status);
-  } else {
-    const picture = await response.json();
-    return picture;
-  }
-};
-
 async function onSearch(e) {
   e.preventDefault();
+
   const { elements } = e.currentTarget;
   const { searchQuery } = elements;
-  console.log(searchQuery.value);
+  name = searchQuery.value
 
   try {
-    const picture = await fetchPixabay();
+    const picture = await fetchPixabay(name);
     const { total, totalHits, hits } = picture;
     console.log(hits);
   } catch {
     console.log("ошибка");
   }
+};
+
+
+btnLoadMore.addEventListener('click', onLoadMore);
+
+async function onLoadMore() {
+  const pictureMore = await fetchPixabay(name);
+  const { total, totalHits, hits } = pictureMore;
+  console.log(hits);
 };
