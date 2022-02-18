d4c48b2ccd33e9ec7d924e35f89b790ec35a2cb3
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2022-02-18 23:17:24 +0200

    ht11

--- src/fetchPixabayserver.js
+++ src/fetchPixabayserver.js
@@ -1,5 +1,6 @@
 export default async function fetchPixabay(name, page, limit) {
-  const response = await fetch(`https://pixabay.com/api/?key=25718667-d0b548046b545cf0dd46ad07c&q=${name}&image_type=photo&safesearch=true&per_page=${limit}&page=${page}`);
+  const APIKEY = '25718667-d0b548046b545cf0dd46ad07c';
+  const response = await fetch(`https://pixabay.com/api/?key=${APIKEY}&q=${name}&image_type=photo&safesearch=true&per_page=${limit}&page=${page}`);
   if (!response.ok) {
     throw new Error(response.status);
   } else {
