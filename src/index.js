c3dbf3645ad1f5356be04e3403ee56e00228fe25
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-03-07 11:03:46 +0200

    Revert "Update index.js"
    This reverts commit a7eb4d6ad9d5487e77265712bd9020082a8b87d1.

--- src/index.js
+++ src/index.js
@@ -7,19 +7,19 @@ import simpleLightbox from './simpleLightbox';
 import renderPosts from './renderPosts';
 
 Loading.circle('Loading...', {
-  backgroundColor: 'rgba(0,0,0,0.8)',
+backgroundColor: 'rgba(0,0,0,0.8)',
 });
 
 setTimeout(() => {
   Loading.remove();
 }, 500);
 
-const form = document.querySelector('#search-form');
+const form = document.querySelector("#search-form");
 const submit = document.querySelector('[type="submit"]');
-const btnLoadMore = document.querySelector('.load-more');
-const gallery = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector(".load-more");
+const gallery = document.querySelector(".gallery");
 
-btnLoadMore.classList.add('is-hidden');
+btnLoadMore.classList.add("is-hidden");
 
 let name = '';
 let page = 1;
@@ -28,6 +28,7 @@ let disabled = false;
 
 async function doStuff(name, page) {
   try {
+
     Loading.circle('Loading...', {
       backgroundColor: 'rgba(0,0,0,0.8)',
     });
@@ -37,29 +38,32 @@ async function doStuff(name, page) {
 
     if (!hits.length) {
       Loading.remove();
-      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      Notify.failure("Sorry, there are no images matching your search query. Please try again.");
 
-      btnLoadMore.classList.add('is-hidden');
+      btnLoadMore.classList.add("is-hidden");
 
       setTimeout(() => {
         gallery.innerHTML = '';
       }, 300);
 
       return;
-    } else if (hits.length < limit || page === 1) {
+
+    } else if (hits.length < limit && page === 1) {
       Loading.remove();
       Notify.success(`Hooray! We found ${totalHits} images.`);
 
-      btnLoadMore.classList.add('is-hidden');
+      btnLoadMore.classList.add("is-hidden");
 
       setTimeout(() => {
         gallery.innerHTML = '';
       }, 300);
+
     } else if (hits.length < limit) {
       Loading.remove();
       Notify.info("We're sorry, but you've reached the end of search results.");
 
-      btnLoadMore.classList.add('is-hidden');
+      btnLoadMore.classList.add("is-hidden");
+
     } else if (page === 1) {
       Loading.remove();
       Notify.success(`Hooray! We found ${totalHits} images.`);
@@ -67,39 +71,42 @@ async function doStuff(name, page) {
       gallery.innerHTML = '';
 
       setTimeout(() => {
-        btnLoadMore.classList.remove('is-hidden');
+        btnLoadMore.classList.remove("is-hidden");
       }, 500);
+
     } else {
       setTimeout(() => {
-        btnLoadMore.classList.remove('is-hidden');
+        btnLoadMore.classList.remove("is-hidden");
       }, 500);
-    }
+    };
 
     setTimeout(() => {
-      gallery.insertAdjacentHTML('beforeend', renderPosts(hits));
+      gallery.insertAdjacentHTML("beforeend", renderPosts(hits));
 
       simpleLightbox.refresh();
 
       setTimeout(() => {
-        const photoCards = document.querySelectorAll('.photo-card');
+        const photoCards = document.querySelectorAll(".photo-card");
 
         if (hits.length > 1) {
           photoCards.forEach(photoCard => {
-            photoCard.classList.remove('is-hidden');
+            photoCard.classList.remove("is-hidden");
           });
         } else if (hits.length === 1) {
           const lastElementArray = photoCards[photoCards.length - 1];
 
-          lastElementArray.classList.remove('is-hidden');
-        }
+          lastElementArray.classList.remove("is-hidden");
+        };
       }, 200);
     }, 300);
+
   } catch (error) {
     console.log(error.message);
-  }
-}
+  };
+};
 
-form.addEventListener('submit', onSearch);
+
+form.addEventListener("submit", onSearch);
 
 function onSearch(e) {
   e.preventDefault();
@@ -110,32 +117,32 @@ function onSearch(e) {
 
   if (disabled) {
     return;
-  }
+  };
 
   disabled = true;
 
-  submit.setAttribute('disabled', 'disabled');
+  submit.setAttribute("disabled", "disabled");
 
   setTimeout(() => {
-    submit.removeAttribute('disabled');
+    submit.removeAttribute("disabled");
     disabled = false;
   }, 1000);
 
-  btnLoadMore.classList.add('is-hidden');
+  btnLoadMore.classList.add("is-hidden");
 
   const { elements } = e.currentTarget;
   const { searchQuery } = elements;
 
   if (!searchQuery.value) {
     Loading.remove(1000);
-    Notify.warning('line is empty');
+    Notify.warning("line is empty");
 
     setTimeout(() => {
       gallery.innerHTML = '';
     }, 300);
 
-    return;
-  }
+    return
+  };
 
   name = searchQuery.value;
   page = 1;
@@ -145,22 +152,24 @@ function onSearch(e) {
   }, 300);
 
   searchQuery.value = '';
-}
+};
+
 
 btnLoadMore.addEventListener('click', debounce(onLoadMore, 300));
 
 function onLoadMore() {
-  btnLoadMore.classList.add('is-hidden');
-
+  btnLoadMore.classList.add("is-hidden");
+  
   page += 1;
 
   doStuff(name, page);
-}
+};
+
 
 gallery.addEventListener('click', evt => {
   evt.preventDefault();
 
   if (!evt.target.classList.contains('gallery__image')) {
     return;
-  }
+  };
 });
